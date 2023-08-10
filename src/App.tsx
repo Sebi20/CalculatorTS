63c690da81f7962dd1cecba0650a18171511ec1b
Sebi20 <sebastian.stephenson1@gmail.com>
2023-08-10 15:11:35 -0400

    Created the input section, closes #1

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,37 @@ const Heading = styled.h1`
   color: red;
 `
 
+const Calculator = styled.div`
+  background-color: #cfcaca;
+  height: 500px;
+  width: 300px;
+  border-radius: 4%;
+  margin: auto;
+  text-align: center;
+  
+`
+
+const Input = styled.input.attrs({ 
+  type: 'text',
+})`
+  margin-top:10px;
+  height:30px;
+  width: 200px;
+  background-color: #494646;
+  border-radius:5%;
+  color:green;
+  
+`
+
 function App() {
   return (
     <>
     <Heading> Calculator app</Heading>
+
+    <Calculator>
+      <Input></Input>
+
+    </Calculator>
     </>
   );
 }
